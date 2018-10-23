0680a12e86a3e5727cf8ef30704c8ff0456afff3
vv <vv@vvdeiMac.local>
2018-10-23 10:07:27 +0800

    初始commit

---
+++ Source/DBJ_AR/DBJ_AR.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "DBJ_AR.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, DBJ_AR, "DBJ_AR" );
---
+++ Source/DBJ_AR/DBJ_ARGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "DBJ_ARGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DBJ_AR_API ADBJ_ARGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
