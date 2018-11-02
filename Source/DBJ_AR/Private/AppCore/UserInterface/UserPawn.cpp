e885746b1fa387556a2ab7b21112ee339b8c3705
vv <vv@vvdeiMac.local>
2018-11-02 21:15:22 +0800

    ar

--- Source/DBJ_AR/Private/AppCore/UserInterface/UserPawn.cpp
+++ Source/DBJ_AR/Private/AppCore/UserInterface/UserPawn.cpp
@@ -31,46 +31,48 @@ void AUserPawn::On_Init()
 }
 void AUserPawn::On_Start()
 {
-	m_Controller = Cast<AUserController>(Controller);
+//    m_Controller = Cast<AUserController>(Controller);
 	UE_LOG(LogTemp, Log, TEXT("zhx : user pawn start."));
 }
 void AUserPawn::On_Tick(float delta)
 {
-	
-	if (m_ARIsSetUP && m_IsPressed)
-	{
-		if (m_SelectActor)
-		{
-			if (m_FingerNum == 1)
-			{
-				AActor * actor = nullptr;
-				m_ScreenPosition = GetFingerPosition(1);
-				if (IsHaveActorInScreenPosition(actor, m_ScreenPosition) && m_SelectActor == actor)
-				{
-					MoveSelecteARActor();
-				}
-				else
-				{
-					m_SelectActor = actor;
-				}
-			}else if (m_FingerNum == 2)
-			{
-				RotateSelectARActor();
-			}
-		}
-		else
-		{
-			if (m_FingerNum == 1)
-			{
-				m_ScreenPosition = GetFingerPosition(1);
-				if (!IsHaveActorInScreenPosition(m_SelectActor,m_ScreenPosition))
-				{
-					m_SelectActor = TryCreateARActor(m_ScreenPosition);
-				}
-			}
-		}
-
-	}
+//
+//    if (m_ARIsSetUP && m_IsPressed)
+//    {
+//        if (m_SelectActor)
+//        {
+//            if (m_FingerNum == 1)
+//            {
+//                m_ScreenPosition = GetFingerPosition(1);
+//                AActor * actor = IsHaveActorInScreenPosition(m_ScreenPosition);
+//
+//                if (m_SelectActor == actor)
+//                {
+//                    MoveSelecteARActor();
+//                }
+////                else
+////                {
+////                    m_SelectActor = actor;
+////                }
+//            }
+////            else if (m_FingerNum == 2)
+////            {
+////                RotateSelectARActor();
+////            }
+//        }
+//        else
+//        {
+//            if (m_FingerNum == 1)
+//            {
+//                m_ScreenPosition = GetFingerPosition(1);
+//                if (!IsHaveActorInScreenPosition(m_ScreenPosition))
+//                {
+//                    m_SelectActor = TryCreateARActor(m_ScreenPosition);
+//                }
+//            }
+//        }
+//
+//    }
 
 }
 void AUserPawn::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
@@ -92,22 +94,22 @@ void AUserPawn::OnFingerTouchPressed(const ETouchIndex::Type FingerIndex, const 
 {
     UE_LOG(LogTemp, Log, TEXT("zhx :: AUserPawn::OnFingerTouch"));
 
-	switch (FingerIndex)
-	{
-		case ETouchIndex::Touch1:
-		{
-			m_IsPressed = true;
-			m_FingerNum++;
-			
-		}break;
-		case ETouchIndex::Touch2:
-		{
-			m_IsPressed = true;
-			m_FingerNum++;
-		}break;
-		default:
-			break;
-	}
+//    switch (FingerIndex)
+//    {
+//        case ETouchIndex::Touch1:
+//        {
+//            m_IsPressed = true;
+//            m_FingerNum++;
+//
+//        }break;
+//        case ETouchIndex::Touch2:
+//        {
+//            m_IsPressed = true;
+//            m_FingerNum++;
+//        }break;
+//        default:
+//            break;
+//    }
 	
 //    FHitResult HitResult;
 //    ((AUserController*)Controller)->GetHitResultUnderFinger(FingerIndex, ECC_Visibility, true, HitResult);
@@ -120,11 +122,11 @@ void AUserPawn::OnFingerTouchPressed(const ETouchIndex::Type FingerIndex, const 
 }
 void AUserPawn::OnFingerTouchReleased(const ETouchIndex::Type FingerIndex, const FVector Location)
 {
-	m_FingerNum--;
-	if (m_FingerNum <= 0)
-	{
-		m_IsPressed = false;
-	}
+//    m_FingerNum--;
+//    if (m_FingerNum <= 0)
+//    {
+//        m_IsPressed = false;
+//    }
 }
 void AUserPawn::OneFingerPress()
 {
@@ -143,82 +145,80 @@ void AUserPawn::oneFingerReleased()
 	m_IsPressed = false;
 
 }
-
-FVector2D AUserPawn::GetFingerPosition(int _fingerNum)
-{
-	FVector2D SPostion;
-	switch (_fingerNum)
-	{
-	case 1:
-	{
-		FVector2D oneFgLocation;
-		bool pressed;
-		m_Controller->GetInputTouchState(ETouchIndex::Touch1, oneFgLocation.X, oneFgLocation.Y, pressed);
-		SPostion = oneFgLocation;
-		
-	}break;
-	case 2:
-	{
-		FVector2D oneFgLocation, twoFgLocation;
-		bool pressed;
-		m_Controller->GetInputTouchState(ETouchIndex::Touch1, oneFgLocation.X, oneFgLocation.Y, pressed);
-		m_Controller->GetInputTouchState(ETouchIndex::Touch2, twoFgLocation.X, twoFgLocation.Y, pressed);
-		SPostion = (oneFgLocation + twoFgLocation) * 0.5;
-	}break;
-	default:
-		break;
-	}
-	return SPostion;
-}
-
-bool AUserPawn::IsHaveActorInScreenPosition(AActor * _outActor, FVector2D _position)
-{
-	bool isHave = false;
-	FHitResult hitResult;
-
-	if (m_Controller->GetHitResultAtScreenPosition(_position, ECC_WorldStatic, false, hitResult))
-	{
-		isHave = true;
-		_outActor = hitResult.GetActor();
-	}
-	//_outActor->GetName();
-
-	return isHave;
-}
-
-AActor * AUserPawn::TryCreateARActor(FVector2D _screenPosition)
-{
-	AActor * actor = nullptr;
-
-
-	return actor;
-}
-
-void AUserPawn::MoveSelecteARActor()
-{
-	
-	if (m_SelectActor && m_SelectActor->IsValidLowLevel())
-	{
-		
-	}
-
-}
-
-void AUserPawn::RotateSelectARActor()
-{
-
-}
-//bool AUserPawn::GetPressedFinger()
-//{
-//	for (int i = 0; i < 10; i++)
-//	{
-//		ETouchIndex::Type index = ETouchIndex::Touch1 + i;
-//		float _x, _y;
-//		bool pressed = false;
-//		m_Controller->GetInputTouchState(index, _x, _y, pressed);
-//		if ()
-//		{
 //
-//		}
-//	}
+//FVector2D AUserPawn::GetFingerPosition(int _fingerNum)
+//{
+//    FVector2D SPostion;
+//    switch (_fingerNum)
+//    {
+//    case 1:
+//    {
+//        FVector2D oneFgLocation;
+//        bool pressed;
+//        m_Controller->GetInputTouchState(ETouchIndex::Touch1, oneFgLocation.X, oneFgLocation.Y, pressed);
+//        SPostion = oneFgLocation;
+//        
+//    }break;
+//    case 2:
+//    {
+//        FVector2D oneFgLocation, twoFgLocation;
+//        bool pressed;
+//        m_Controller->GetInputTouchState(ETouchIndex::Touch1, oneFgLocation.X, oneFgLocation.Y, pressed);
+//        m_Controller->GetInputTouchState(ETouchIndex::Touch2, twoFgLocation.X, twoFgLocation.Y, pressed);
+//        SPostion = (oneFgLocation + twoFgLocation) * 0.5;
+//    }break;
+//    default:
+//        break;
+//    }
+//    return SPostion;
 //}
+//
+//AActor *  AUserPawn::IsHaveActorInScreenPosition(FVector2D _position)
+//{
+//    AActor * actor = nullptr;
+//    FHitResult hitResult;
+//
+//    if (m_Controller->GetHitResultAtScreenPosition(_position, ECC_WorldStatic, false, hitResult))
+//    {
+//        actor = hitResult.GetActor();
+//    }
+//
+//    return actor;
+//}
+//
+//AActor * AUserPawn::TryCreateARActor(FVector2D _screenPosition)
+//{
+//    AActor * actor = nullptr;
+//
+//
+//    return actor;
+//}
+//
+//void AUserPawn::MoveSelecteARActor()
+//{
+//    
+//    if (m_SelectActor && m_SelectActor->IsValidLowLevel())
+//    {
+//        
+//    }
+//
+//}
+//
+//void AUserPawn::RotateSelectARActor()
+//{
+//
+//}
+////bool AUserPawn::GetPressedFinger()
+////{
+////    for (int i = 0; i < 10; i++)
+////    {
+////        ETouchIndex::Type index = ETouchIndex::Touch1 + i;
+////        float _x, _y;
+////        bool pressed = false;
+////        m_Controller->GetInputTouchState(index, _x, _y, pressed);
+////        if ()
+////        {
+////
+////        }
+////    }
+////}
--- Source/DBJ_AR/Public/AppCore/UserInterface/UserPawn.h
+++ Source/DBJ_AR/Public/AppCore/UserInterface/UserPawn.h
@@ -37,44 +37,46 @@ public:
 	void OnFingerTouchReleased(const ETouchIndex::Type FingerIndex, const FVector Location);
 
 public:
-   
-	//
-	UFUNCTION(BlueprintImplementableEvent)
-		void StartARSession();
-	UFUNCTION(BlueprintImplementableEvent)
-		bool GetTrackedGeometryTransform(FVector2D _scrrenPosition,FTransform & _transform);
-
-	UFUNCTION(BlueprintCallable)
-		FVector2D	GetFingerPosition(int _fingerNum);
-	UFUNCTION(BlueprintCallable)
-		bool IsHaveActorInScreenPosition(AActor * _outActor, FVector2D _position);
-	UFUNCTION(BlueprintCallable)
-		AActor * TryCreateARActor(FVector2D _screenPosition);
-	UFUNCTION(BlueprintCallable)
-		void MoveSelecteARActor();
-	UFUNCTION(BlueprintCallable)
-		void RotateSelectARActor();
-	/*UFUNCTION(BlueprintCallable)
-		bool GetPressedFinger();*/
-
+//   
+//    //
+    UFUNCTION(BlueprintImplementableEvent)
+        void StartARSession();
+    UFUNCTION(BlueprintImplementableEvent)
+        void DrawPlnes();
+    UFUNCTION(BlueprintImplementableEvent)
+        bool GetTrackedGeometryTransform(FVector2D _scrrenPosition,FTransform & _transform);
+//
+//    UFUNCTION(BlueprintCallable)
+//        FVector2D    GetFingerPosition(int _fingerNum);
+//    UFUNCTION(BlueprintCallable)
+//        AActor * IsHaveActorInScreenPosition(FVector2D _position);
+//    UFUNCTION(BlueprintCallable)
+//        AActor * TryCreateARActor(FVector2D _screenPosition);
+//    UFUNCTION(BlueprintCallable)
+//        void MoveSelecteARActor();
+//    UFUNCTION(BlueprintCallable)
+//        void RotateSelectARActor();
+//    /*UFUNCTION(BlueprintCallable)
+//        bool GetPressedFinger();*/
+//
 public:
-	UPROPERTY(VisibleAnywhere)
-		UCameraComponent * m_Camera;
+    UPROPERTY(VisibleAnywhere)
+        UCameraComponent * m_Camera;
 
-	UPROPERTY(VisibleAnywhere, BlueprintReadWrite)
-		bool  m_ARIsSetUP;
-	UPROPERTY(VisibleAnywhere, BlueprintReadWrite)
-		bool  m_IsPressed;
-	UPROPERTY(VisibleAnywhere, BlueprintReadWrite)
-		int32  m_FingerNum;
-	UPROPERTY(VisibleAnywhere, BlueprintReadWrite)
-		FVector2D m_ScreenPosition;
-	UPROPERTY(VisibleAnywhere, BlueprintReadWrite)
-		AActor * m_SelectActor;
-
-	UPROPERTY(VisibleAnywhere, BlueprintReadWrite)
-	TArray<TEnumAsByte<ETouchIndex::Type>>	m_PressedFingers;
+    UPROPERTY(VisibleAnywhere, BlueprintReadWrite)
+        bool  m_ARIsSetUP;
+    UPROPERTY(VisibleAnywhere, BlueprintReadWrite)
+        bool  m_IsPressed;
+    UPROPERTY(VisibleAnywhere, BlueprintReadWrite)
+        int32  m_FingerNum;
+    UPROPERTY(VisibleAnywhere, BlueprintReadWrite)
+        FVector2D m_ScreenPosition;
+    UPROPERTY(VisibleAnywhere, BlueprintReadWrite)
+        AActor * m_SelectActor;
+//
+    UPROPERTY(VisibleAnywhere, BlueprintReadWrite)
+    TArray<TEnumAsByte<ETouchIndex::Type>>    m_PressedFingers;
 
 private:
-	AUserController * m_Controller;
+    AUserController * m_Controller;
 };
